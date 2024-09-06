080d0efee4972beb79f1adf800fc4ee753bfe1ca
TeachDian <philiptenibas@gmail.com>
2024-09-06 17:10:55 +0800

    787

--- train_yolo.py
+++ train_yolo.py
@@ -2,7 +2,7 @@ import torch
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8 model (e.g., 'yolov8s.pt' for YOLOv8 small)
-model = YOLO('yolovSabinet.pt')
+model = YOLO('models/3epoch.pt')
 
 # Train the model on your custom dataset
 results = model.train(data='data.yaml', epochs=3, imgsz=640, device='cuda' if torch.cuda.is_available() else 'cpu')
